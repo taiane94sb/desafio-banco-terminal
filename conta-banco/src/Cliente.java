4adc5ec0db9e191be3a6c761969f6fb084823635
taiane94sb <taiane19sb94@gmail.com>
2023-10-10 17:34:13 -0300

    projeto conta banco

---
+++ conta-banco/src/Cliente.java
@@ -0,0 +1,6 @@
+public class Cliente {
+    String name;
+    int agencia;
+    int conta;
+    double saldo;
+}
--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,40 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        Cliente cliente = new Cliente();
+
+        String name = "";
+        int agencia = 0;
+        int conta = 0;
+        double saldo = 0;
+
+        try {
+            System.out.println("Digite seu nome: ");
+            name = scanner.nextLine();
+
+            System.out.println("Digite sua agencia: ");
+            agencia = scanner.nextInt();
+
+            System.out.println("Digite seu numero da sua conta: ");
+            conta = scanner.nextInt();
+
+            System.out.println("Qual o valor que deseja depósitar? ");
+            saldo = scanner.nextDouble();
+
+            cliente.name = name;
+            cliente.agencia = agencia;
+            cliente.conta = conta;
+            cliente.saldo = saldo;
+
+            System.out.println("Olá," + name);
+            System.out.println("Agência: " + agencia
+                    + ", Conta: " + conta + ", Saldo: R$ " + saldo);
+        } finally {
+            scanner.close();
+        }
     }
 }
